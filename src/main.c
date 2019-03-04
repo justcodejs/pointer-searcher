7ef4af971302b08001be9547d07e34c0a3ee2956
justcodejs <gabrylanz@gmail.com>
2019-03-04 15:29:18 +0100

    Fixed bug of diffPoint

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ typedef struct Point
 {
 	float x;
 	float y;
+	Point *p;
 } Point;
 
 int main(int argc, char *argv[])
@@ -31,16 +32,25 @@ int main(int argc, char *argv[])
 	float diffPoint[x/2];
 	for (int i = 0; i < x / 2; i++)
 	{
-		diffPoint[i] = point[i].x - point[i+1].y;
+		diffPoint[i] = point[i].x - point[i].y;
 	}
 
-	for (int i = 0; i < (sizeof(diffPoint)/sizeof(*diffPoint))/2; i++)
+
+	for (int i = 0; i < x/2; i++)
 	{
-		float a = pow(diffPoint[i], 2);
-		float b = pow(diffPoint[i+1], 2);
-		float d = sqrt(a+b);
-		printf("Point: %.1f\n", d);
+		printf("Point: %.1f\n", diffPoint[i]);
 	}
 
+	float a, b, d;
+	/* for (int i = 0; i < (sizeof(diffPoint)/sizeof(*diffPoint))/2; i++)
+	{
+		a = pow(diffPoint[i], 2);
+		b = pow(diffPoint[i+1], 2);
+		d = sqrt(a+b);
+		printf("Point: %.1f\n", d);
+	} */
+
+	printf("\nEnd program.\n");
+	getchar();
 	return 0;
 }
