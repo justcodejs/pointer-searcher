c06125bcaf411d44f08326fdb00ec798caadde4e
justcodejs <gabrylanz@gmail.com>
2019-03-06 19:20:16 +0100

    Tab size changed

--- src/main.c
+++ src/main.c
@@ -1,19 +1,16 @@
 #include "base_include.h"
 
-typedef struct Point
-{
-	double x;
-	double y;
+typedef struct Point {
+	double x; // Asse X
+	double y; // Asse Y
 } Point;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	FILE *in = fopen("data/input.txt", "r");
 	double values[1024];
 	int x = 0;
 
-	while (!feof(in))
-	{
+	while (!feof(in)) {
 		fscanf(in, "%lf", &values[x]);
 		x++;
 	}
@@ -25,8 +22,7 @@ int main(int argc, char *argv[])
 	int _x = 0;
 	int _y = 1;
 
-	for (int j = 0; j < x; j++)
-	{
+	for (int j = 0; j < x; j++) {
 		point[j].x = values[j + _x];
 		point[j].y = values[j + _y];
 
@@ -38,8 +34,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int _i = 1;
 
-	while ((i+_i) != x/2)
-	{
+	while ((i+_i) != x/2) {
 		diffPoint[i] = point[i].x - point[i + _i].x;
 		diffPoint[i + _i] = point[i].y - point[i + _i].y;
 		i++;
@@ -47,8 +42,7 @@ int main(int argc, char *argv[])
 	
 	double a, b, d;
 
-	for (int i = 0; i < (sizeof(diffPoint)/sizeof(*diffPoint))/2; i++)
-	{
+	for (int i = 0; i < (sizeof(diffPoint)/sizeof(*diffPoint))/2; i++) {
 		a = diffPoint[i] * diffPoint[i];
 		b = diffPoint[i+1] * diffPoint[i+1];
 		d = a + b;
